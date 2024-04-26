00491ec0a1d8ffcd601544d83f3cb5afb1dc7296
lavahang <llavahang@gmail.com>
2024-04-26 17:19:31 +0545

    initial

---
+++ src/components/Todo.js
@@ -0,0 +1,40 @@
+"use client"
+import React from 'react'
+import { useState } from 'react';
+const Todo = () => {
+  const [message, setMessage] = useState('');
+  const [update,setupdate]= useState(message)
+  const handlechange = (event) => {
+    // 👇 "message" stores input field value
+    setMessage(event.target.value);
+  };
+  const handleclick = () => {
+    // 👇 "message" to store output
+    setupdate(message);
+    
+  };
+
+
+  return (
+    <>
+    <div>
+    <h1>Todo app</h1>
+    
+    <input type="text" value={message} onChange={handlechange}/>
+
+    <button onClick={handleclick} >Add</button>
+    <button>Delete</button>
+
+    <div>
+    {update}
+    </div>
+
+
+
+
+    </div>
+    </>
+  )
+}
+
+export default Todo
